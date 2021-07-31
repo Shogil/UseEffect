c73fc9f36889d6710541852a487c7db4b8b0051d
Shogil <shogilbabu@gmail.com>
2021-07-31 16:13:42 +0530

    update the mounting content

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+
 
 function Counter() {
+    useEffect(()=>{
+          console.log('Mounting...')
+          return ()=>{
+              console.log('Unmounting...')
+          }
+    })
+    const [count,setCount] = useState(0)
     return (
         <div>
-            <h1> Hello I am Component</h1>
+            <button onClick={()=>setCount(count+1)}>Add</button>
+            <h1> Hello I am Component: {count}</h1>
             
         </div>
     )
